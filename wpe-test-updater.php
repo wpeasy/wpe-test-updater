ba37283812a4a52abdc16900d81cb5b052f9511e
Alan Blair <alan.blair@plana.com.au>
2019-12-28 08:45:20 +1000

    Update to 1.0.4

--- wpe-test-updater.php
+++ wpe-test-updater.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPEasy Test Plugin
  * Plugin URI:
  * Description: Test to see how plugin updates via Github works
- * Version: 1.0.1
+ * Version: 1.0.4
  * Author: Alan Blair
  * Author URI:
  * Text Domain: wpeasy
